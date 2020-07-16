0ee797d434e007193fb42c4830b6c4e8dd3238c2
MrLukashem <mertalukasz@gmail.com>
2020-07-16 17:47:36 +0200

    build folder content has been removed

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lmerta/Projects/wasabi
-# Build directory: /home/lmerta/Projects/wasabi/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(wasabiTest "/home/lmerta/Projects/wasabi/build/wasabi")
-set_tests_properties(wasabiTest PROPERTIES  _BACKTRACE_TRIPLES "/home/lmerta/Projects/wasabi/CMakeLists.txt;31;add_test;/home/lmerta/Projects/wasabi/CMakeLists.txt;0;")
